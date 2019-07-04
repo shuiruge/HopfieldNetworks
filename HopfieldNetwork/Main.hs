eb68f27a0d0c0b8d06f4b2637471392837dd79d0
shuiruge <shuiruge@hotmail.com>
2019-07-04 11:54:31 +0800

    Fix bug.

--- HopfieldNetwork/Main.hs
+++ HopfieldNetwork/Main.hs
@@ -11,18 +11,18 @@ memory = fromBits <$> ["1010101", "010101"]
 hopfield :: Hopfield
 hopfield = foldl (memorize learningRate) emptyHopfield memory
 
-iterate :: Int -> State -> Writer [String] State
-iterate epochs state
+iterate' :: Int -> State -> Writer [String] State
+iterate' epochs state
     | epochs == 0 = do
         tell [show state]
-        return $ state
+        return state
     | otherwise = do
         let newState = ordinalAsynUpdate hopfield state
         tell [show newState]
-        iterate (epochs - 1) newState
+        iterate' (epochs - 1) newState
 
 main :: IO ()
 main = do
     let epoch = 5
         initState = fromBits "0000000"
-    mapM_ putStrLn $ snd $ runWriter $ iter epoch initState
+    mapM_ putStrLn $ snd $ runWriter $ iterate' epoch initState
